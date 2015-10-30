0940d8237a0a5dfca058a5a0dd44004336ed0c86
Dayo <deetu02@gmail.com>
2015-10-30 01:44:57 +0000

    Added new files and setting up the default web theme and feel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require jquery.datetimepicker
 //= require_tree .
---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :name, :Description, :created_at, :updated_at
